1aa909fd141353ba9ea0b87c0668fd4cac072646
fiqrijml <jamalfiqri@gmail.com>
2020-04-02 15:14:33 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+import { Link } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav style={ linkStyle }>
+        <ul>
+          <li><Link style={ linkStyle } to="/">Home</Link></li>  
+          <li><Link style={ linkStyle } to="/about">About</Link></li>  
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
     </div>
+    </Router>
   );
 }
-
+const linkStyle = {
+  color: 'black',
+  textDecoration: 'none',
+}
 export default App;
